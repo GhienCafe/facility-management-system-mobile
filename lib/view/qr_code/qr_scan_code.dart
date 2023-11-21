d16388656c53ce717f4535a9fabea2b3c1d1d0c2
Tunguyen261 <nguyendoantu2610@gmail.com>
2023-11-21 20:55:45 +0700

    edit QR check asset

--- lib/view/qr_code/qr_scan_code.dart
+++ lib/view/qr_code/qr_scan_code.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:get/get.dart';
 
+import '../../res/color/colors.dart';
 import '../../utlis/utlis.dart';
 import '../../view_models/controller/qr/qr_controller.dart';
 
@@ -19,103 +20,122 @@ class _QRViewExampleState extends State<QRViewExample> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   final qrController = Get.put(QRController());
 
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('QR Code'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(flex: 4, child: _buildQrView(context)),
-          Expanded(
-            flex: 1,
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  if (result != null)
-                    Text(
-                      'Data: ${result!.code}',
-                      style: const TextStyle(
-                          fontSize: 12, fontWeight: FontWeight.bold),
-                    )
-                  else
-                    const Text(
-                      'Scan a code',
-                      style:
-                          TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-                    ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.all(5),
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              await controller?.toggleFlash();
-                              setState(() {});
-                            },
-                            child: FutureBuilder(
-                              future: controller?.getFlashStatus(),
-                              builder: (context, snapshot) {
-                                if (snapshot.data == true) {
-                                  return const Icon(Icons.flash_on);
-                                } else {
-                                  return const Icon(Icons.flash_off);
-                                }
-                              },
-                            )),
-                      ),
-                      const SizedBox(width: 10),
-                      Container(
-                        margin: const EdgeInsets.all(5),
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              await controller?.flipCamera();
-                              setState(() {});
-                            },
-                            child: FutureBuilder(
-                              future: controller?.getCameraInfo(),
-                              builder: (context, snapshot) {
-                                if (snapshot.data != null) {
-                                  return const Icon(Icons.cameraswitch);
-                                } else {
-                                  return const Text('loading');
-                                }
-                              },
-                            )),
-                      ),
-                      const SizedBox(width: 10),
-                      Container(
-                        margin: const EdgeInsets.all(5),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            await controller?.pauseCamera();
-                          },
-                          child: const Icon(Icons.no_photography),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      Container(
-                        margin: const EdgeInsets.all(5),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            await controller?.resumeCamera();
-                          },
-                          child: const Icon(Icons.local_see),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          leading: const BackButton(color: AppColor.whiteColor),
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              colors: [
+                Color(0xff0e4e86),
+                Color(0xff1461a2),
+                Color(0xff2e7abb),
+              ],
+            )),
+          ),
+          title: const Text(
+            'QR Code',
+            style: TextStyle(
+              color: AppColor.whiteColor,
+              letterSpacing: 1,
             ),
-          )
-        ],
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(flex: 4, child: _buildQrView(context)),
+            Expanded(
+              flex: 1,
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    if (result != null)
+                      Text(
+                        'Data: ${result!.code}',
+                        style: const TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.bold),
+                      )
+                    else
+                      const Text(
+                        'Scan a code',
+                        style: TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.bold),
+                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.all(5),
+                          child: ElevatedButton(
+                              onPressed: () async {
+                                await controller?.toggleFlash();
+                                setState(() {});
+                              },
+                              child: FutureBuilder(
+                                future: controller?.getFlashStatus(),
+                                builder: (context, snapshot) {
+                                  if (snapshot.data == true) {
+                                    return const Icon(Icons.flash_on);
+                                  } else {
+                                    return const Icon(Icons.flash_off);
+                                  }
+                                },
+                              )),
+                        ),
+                        const SizedBox(width: 10),
+                        Container(
+                          margin: const EdgeInsets.all(5),
+                          child: ElevatedButton(
+                              onPressed: () async {
+                                await controller?.flipCamera();
+                                setState(() {});
+                              },
+                              child: FutureBuilder(
+                                future: controller?.getCameraInfo(),
+                                builder: (context, snapshot) {
+                                  if (snapshot.data != null) {
+                                    return const Icon(Icons.cameraswitch);
+                                  } else {
+                                    return const Text('loading');
+                                  }
+                                },
+                              )),
+                        ),
+                        const SizedBox(width: 10),
+                        Container(
+                          margin: const EdgeInsets.all(5),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.pauseCamera();
+                            },
+                            child: const Icon(Icons.no_photography),
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Container(
+                          margin: const EdgeInsets.all(5),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.resumeCamera();
+                            },
+                            child: const Icon(Icons.local_see),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -144,10 +164,10 @@ class _QRViewExampleState extends State<QRViewExample> {
       controller.pauseCamera();
       result = scanData;
       final String? qrCode = scanData.code;
-      if(qrCode == widget.taskInfoId){
+      if (qrCode == widget.taskInfoId) {
         Get.back();
         Utils.snackBarSuccess("Thông Báo:", "Thiết Bị Trùng Khớp");
-      } else{
+      } else {
         Get.back();
         Utils.snackBarError("Thông Báo:", "Thiết Bị Không Trùng Khớp");
       }
